76f1823e2ba9615a844b9e36a4dc2b6b6d0a18e2
tiagolimarocha <tiago.rocha@radically.digital>
2022-08-18 14:46:45 +0100

    added and integrated rds, vpc and security group modules

--- src/packages/infrastructure/AWS/environments/development/main.tf
+++ src/packages/infrastructure/AWS/environments/development/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = local.region
 
   # Make it faster by skipping something
   skip_get_ec2_platforms      = true
@@ -11,15 +11,136 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+
+locals {
+  name             = "pet-store-infrastructure--example"
+  region           = "eu-west-2"
+  current_identity = data.aws_caller_identity.current.arn
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
 ##############
 # Lambdas Map
 ##############
-locals {
-  LAMBDAS = {
-    type = map(any)
-    default = {
-      "function_name" = "function description"
+variable "LAMBDAS" {
+  type = map(string)
+  default = {
+    "function_name" = "function description"
+  }
+}
+
+#########################
+# VPC and Security Group
+#########################
+
+module "vpc" {
+  source  = "../../modules/vpc"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = "10.99.0.0/18"
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
+
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+
+  tags = local.tags
+}
+
+module "security_group" {
+  source  = "../../modules/security-group"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Complete PostgreSQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.tags
+}
+
+
+###############
+# RDS Instance
+###############
+
+module "db" {
+  source = "../../modules/rds"
+
+  identifier = local.db_name
+
+  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14"
+  major_engine_version = "14"
+  instance_class       = "db.t4g.large"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+
+  db_name  = "completePostgresql"
+  username = "complete_postgresql"
+  port     = 5432
+
+  multi_az               = true
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  create_monitoring_role                = true
+  monitoring_interval                   = 60
+  monitoring_role_name                  = "example-monitoring-role-name"
+  monitoring_role_use_name_prefix       = true
+  monitoring_role_description           = "Description for monitoring role"
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
     }
+  ]
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
   }
 }
 
@@ -28,7 +149,7 @@ locals {
 ############
 
 module "s3_bucket" {
-  for_each = local.LAMBDAS
+  for_each = var.LAMBDAS
 
   source = "../../modules/s3-bucket"
 
@@ -41,7 +162,7 @@ module "s3_bucket" {
 ###############################
 
 module "lambda_layer_s3" {
-  source = "../../"
+  source = "../../modules/lambda"
 
   create_layer = true
 
@@ -61,7 +182,7 @@ module "lambda_layer_s3" {
 ####################################################
 
 module "lambda_function" {
-  for_each = local.LAMBDAS
+  for_each = var.LAMBDAS
 
   source = "../../modules/lambda"
 
